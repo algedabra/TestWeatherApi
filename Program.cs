5583a52667ec16083f392c9796378fad7244b7e9
Daniel Brase <natcho82@freenet.de>
2020-02-28 18:01:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forecast();
+            Console.ReadKey();
+        }
+
+        public static async Task Forecast()
+        {
+            var apiKey = "2362fdd674b94c0c3541af74ebfe2eff";
+            var darkSky = new DarkSky.Services.DarkSkyService(apiKey);
+            var forecast = await darkSky.GetForecast(52.99042, 8.81614);
+
+            if (forecast?.IsSuccessStatus == true)
+            {
+                Console.WriteLine(forecast.Response.Currently.Summary);
+            }
+            else
+            {
+                Console.WriteLine("No current weather data");
+            }
+            Console.WriteLine(forecast.AttributionLine);
+            Console.WriteLine(forecast.DataSource);
+        }
+    }
+}
